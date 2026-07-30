6b33e8cebfbed0a7db85a4765652213c450e0d04
jaiinderveer <jaiinderveersingh@gmail.com>
2026-07-31 02:56:53 +0530

    Modified frontend

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -68,12 +68,12 @@ export default function Contacts() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setFormError('');
-    if(!isNumber(formData.phone) && formData.phone.length < 10){
-      setFormError('Phone Number Invalid');
-      return;
-    }
     if (!formData.name.trim() || !formData.phone.trim()) {
       setFormError('Name and phone are required.');
+      return;
+    }
+    elif(!isNumber(formData.phone) && formData.phone.length < 10){
+      setFormError('Phone Number Invalid');
       return;
     }
 

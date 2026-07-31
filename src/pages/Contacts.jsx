6cbb179cd8da6e3d16cc4fd5cb34975e6224a402
jaiinderveer <jaiinderveersingh@gmail.com>
2026-07-31 16:38:56 +0530

    Modified Frontend

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -80,8 +80,8 @@ export default function Contacts() {
 
     // 2. Check for minimum length OR invalid characters 
     // The regex /^\+?\d+$/ means: optional '+' at the start, followed only by numbers
-    else if (phoneTrimmed.length < 10 || !/^\+?\d+$/.test(phoneTrimmed)) {
-      setFormError('Phone Number Invalid. Must be at least 10 digits.');
+    else if (phoneTrimmed.length < 13 || !/^\+?\d+$/.test(phoneTrimmed)) {
+      setFormError('Phone Number Invalid. Must be at least 10 digits with country code.');
       return;
     }
 
